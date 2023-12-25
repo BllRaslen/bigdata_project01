8efcf93d2ad9e0923ff84eee5c5dedf92d892092
bllRASLEN <eng.bll.raslen@gmail.com>
2023-12-25 14:01:20 +0200

    Run Last version

--- ml.py
+++ ml.py
@@ -1,58 +1,46 @@
 import os
-
-# ml.py
 from pyspark.ml.classification import LogisticRegression
 from pyspark.ml.feature import VectorAssembler
 from pyspark.ml import Pipeline
-from pyspark.sql.functions import col, expr
-import shutil
-import os
+from pyspark.sql.functions import col
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType, StringType, IntegerType, FloatType
 
-os.environ["SPARK_LOCAL_IP"] = "192.168.1.255"
 
 def train_ml_model(static_data):
-    # "label" column in static data
+    # Cast "Quantity" column to IntegerType and use it as the label
     static_data_with_label = static_data.withColumn("label", col("Quantity").cast(IntegerType()))
 
     # Create a feature vector using VectorAssembler
-    assembler = VectorAssembler(inputCols=["Year", "Month", "Quantity", "Pct"], outputCol="Cfeatures")
-    lr = LogisticRegression(featuresCol="Cfeatures", labelCol="label")
+    assembler = VectorAssembler(inputCols=["Year", "Month", "Quantity"], outputCol="features")
+
+    # Logistic Regression model
+    lr = LogisticRegression(featuresCol="features", labelCol="label")
 
     # Create a pipeline for the machine learning model
     pipeline = Pipeline(stages=[assembler, lr])
-    model = pipeline.fit(static_data_with_label)  # Use the static data with the label
 
-    if os.path.exists("/home/bll/PycharmProjects/bigdata_project/metaData"):
-        shutil.rmtree("/home/bll/PycharmProjects/bigdata_project/metaData")
+    # Train and save the machine learning model
+    model = pipeline.fit(static_data_with_label)
+    model.save("/home/bll/PycharmProjects/bigdata_project/LRM/metadata")
 
-    model.save("/home/bll/PycharmProjects/bigdata_project/metaData")
-    # Print the output of the machine learning model without truncation
+    # Transform the data with the trained model
     ml_result = model.transform(static_data_with_label)
-    # ml_result.show(truncate=False, n=ml_result.count())
+
+    # Show the ML result without truncation
+    ml_result.show(truncate=False, n=ml_result.count())
 
     return model
 
+
 if __name__ == "__main__":
-    from pyspark.sql import SparkSession
-    from pyspark.sql.types import StructType, StringType, IntegerType, FloatType
-
-    # Verify PySpark installation
-    try:
-        from pyspark import SparkContext, SparkConf
-
-        print("PySpark is installed and accessible.")
-    except ImportError:
-        print("PySpark is not installed or not accessible.")
-
-
     def create_spark_session():
         return (
             SparkSession.builder.appName("MLTraining")
             .config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.0")
-            .config("spark.driver.host", "localhost")
-            .config("spark.ui.reverseProxy", "true")
-            .config("spark.ui.reverseProxyUrl", "http://localhost:4049")
-            .config("spark.ui.port", "4049")  # Set the SparkUI port explicitly
+            .config("spark.driver.host", "localhost")  # Set the driver host to suppress the hostname warning
+            .config("spark.ui.reverseProxy", "true")  # Enable reverse proxy for Spark UI
+            .config("spark.ui.reverseProxyUrl", "http://localhost:4040")  # Set the reverse proxy URL
             .getOrCreate()
         )
 
@@ -62,13 +50,12 @@ if __name__ == "__main__":
     kafka_topic = "car_topic"
 
     # Define the schema for the data
-    # Define the schema for the data
     schema = StructType().add("Year", IntegerType()).add("Month", IntegerType()) \
         .add("Make", StringType()).add("Model", StringType()) \
         .add("Quantity", IntegerType()).add("Pct", FloatType())
 
     # Read static data from a CSV file
-    static_data_path = "KaggleDataset.csv"
+    static_data_path = "KaggleDataset.csv"  # Adjust the path to your dataset
     static_data = create_spark_session().read.csv(static_data_path, header=True, schema=schema)
 
     # Train the machine learning model
